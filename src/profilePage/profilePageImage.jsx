40137fb60fc82217adb2df6e08bbed3001ad582e
Saksham Gera <sakshamgera1456@gmail.com>
2023-12-23 02:36:45 +0530

    third commit

--- src/profilePage/profilePageImage.jsx
+++ src/profilePage/profilePageImage.jsx
@@ -3,7 +3,7 @@ import "./profilePageImage.css";
 export default function ProfilePageImage() {
     return (
         <div className="profile-page-image">
-            <img src="https://github.com/saksham-gera/portfolio-website/blob/main/src/assets/ProfilePic.jpg"></img>
+            <img src="/src/assets/ProfilePic.jpg"></img>
         </div>
     );
 }
