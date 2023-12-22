5c567ed87a10951c081ad3c7d279f4be4202c971
Saksham Gera <sakshamgera1456@gmail.com>
2023-12-23 02:11:17 +0530

    second deployment with corrected paths

--- src/experiencesPage.jsx
+++ src/experiencesPage.jsx
@@ -6,20 +6,20 @@ export default function ExperiencesPage() {
         <div id="experiences" className="experiences-page">
             <h3>Past Experiences And Projects</h3>
             <div className="experiences-content">
-                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("/src/assets/trialProject.png")' link="sparshsvnit.in"/>
-                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("/src/assets/trialProject.png")' link="sparshsvnit.in"/>
+                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("https://github.com/saksham-gera/portfolio-website/blob/main/src/assets/trialProject.jpg")' link="sparshsvnit.in"/>
+                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("https://github.com/saksham-gera/portfolio-website/blob/main/src/assets/trialProject.jpg")' link="sparshsvnit.in"/>
             </div>
             <div className="experiences-content">
-                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("/src/assets/trialProject.png")' link="sparshsvnit.in"/>
-                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("/src/assets/trialProject.png")' link="sparshsvnit.in"/>
+                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("https://github.com/saksham-gera/portfolio-website/blob/main/src/assets/trialProject.jpg")' link="sparshsvnit.in"/>
+                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("https://github.com/saksham-gera/portfolio-website/blob/main/src/assets/trialProject.jpg")' link="sparshsvnit.in"/>
             </div>
             <div className="experiences-content">
-                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("/src/assets/trialProject.png")' link="sparshsvnit.in"/>
-                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("/src/assets/trialProject.png")' link="sparshsvnit.in"/>
+                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("https://github.com/saksham-gera/portfolio-website/blob/main/src/assets/trialProject.jpg")' link="sparshsvnit.in"/>
+                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("https://github.com/saksham-gera/portfolio-website/blob/main/src/assets/trialProject.jpg")' link="sparshsvnit.in"/>
             </div>
             <div className="experiences-content">
-                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("/src/assets/trialProject.png")' link="sparshsvnit.in"/>
-                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("/src/assets/trialProject.png")' link="sparshsvnit.in"/>
+                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("https://github.com/saksham-gera/portfolio-website/blob/main/src/assets/trialProject.jpg")' link="sparshsvnit.in"/>
+                <ExperienceCard title="SPARSH" description="bye" experienceImageURL='url("https://github.com/saksham-gera/portfolio-website/blob/main/src/assets/trialProject.jpg")' link="sparshsvnit.in"/>
             </div>
         </div>
     );
--- src/profileImage.jsx
+++ src/profileImage.jsx
@@ -3,7 +3,7 @@ import './profileImage.css';
 export default function ProfileImage() {
     return (
         <div className="profile-image">
-            <img src="/src/assets/ProfilePic.jpg"></img>
+            <img src="https://github.com/saksham-gera/portfolio-website/blob/main/src/assets/ProfilePic.jpg"></img>
         </div>
     );
 }
